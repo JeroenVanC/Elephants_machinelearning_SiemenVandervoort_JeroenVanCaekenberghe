86a6a2ee8a8b41a3eabb02832bebd2b2311aacb8
siemenvdv <siemen.vandervoort@student.uhasselt.be>
2021-11-18 14:30:13 +0100

    Afbeeldingen random in de lijst zetten

--- Code/main.py
+++ Code/main.py
@@ -2,6 +2,7 @@
 from PIL import Image
 import imageio as iio
 import numpy as np
+import random
 
 # Constants
 newSize = 50
@@ -39,33 +40,48 @@ def cropimages():
 
 	print("Images cropped!");
 
-
 def loadimages():
 	print("Loading images ...")
 	train_set_x = []
 	train_set_y = []
+	for i in range(0, 2 * train_images):
+		train_set_x.append(0)
+		train_set_y.append(0)
 
-	# Load Asian images
+	#Generate list with random numbers
+	randomList = []
+	randomList = random.sample(range(0, (2 * train_images)), 2 * train_images)
+
+	#Use a counter to iterate through the randomList
+	counter = 0
+
+	# Load African images
 	for number in range(1, train_images + 1):
 		Af_rgb = iio.imread("Dataset/Train/Resized_Images/African/African_" + str(number) + ".jpg")
-		train_set_x.append(Af_rgb)
-		train_set_y.append(1.0)
+		train_set_x[randomList[counter]] = Af_rgb
+		train_set_y[randomList[counter]] = 1.0
+		counter = counter + 1
 
 	# Load Asian images
 	for number in range(1, train_images + 1):
 		As_rgb = iio.imread("Dataset/Train/Resized_Images/Asian/Asian_" + str(number) + ".jpg")
-		train_set_x.append(As_rgb)
-		train_set_y.append(0.0)
+		train_set_x[randomList[counter]] = As_rgb
+		train_set_y[randomList[counter]] = 0.0
+		counter = counter + 1
 
 	print("Images loaded!")
 
 	return train_set_x, train_set_y
 
-
 if __name__ == '__main__':
-	# cropimages()
+
+	#Crop the images
+	#cropimages()
+
+	#Load the images into lists
 	train_set_x, train_set_y = loadimages()
 
+	#Convert the lists to arrays
 	train_set_x = np.array(train_set_x, dtype="object")
 	train_set_y = np.array(train_set_y, dtype="object")
 
